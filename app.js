057e9f52e0d862dc209eecb576c9eac345532d2a
asraf-mridha <asrafmridha@gmail.com>
2022-07-22 21:12:59 +0600

    add scroll down to top by Jquery

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function(){
+    jQuery(document).ready(function(){
 
         jQuery(".menu ul ul").parent("li").children("a").append('<i class="fa-solid fa-chevron-down"></i>');
 
@@ -44,11 +44,19 @@ jQuery(document).ready(function(){
         if(p >220){
 
                 jQuery("nav").addClass("fixed");
+                jQuery(".top").show();
         }
         else{
 
                 jQuery("nav").removeClass("fixed");
+                jQuery(".top").hide();
         }
+     });
+
+     jQuery(".top").click(function(){
+       
+           jQuery("html").animate({scrollTop:"0"},1000);
+
      });
 
 
