2f6c3f46ee313bd13a18802d5b9463354d1bbf12
asraf-mridha <asrafmridha@gmail.com>
2022-07-22 15:18:50 +0600

    Some responsive for width < 768

--- app.js
+++ app.js
@@ -3,7 +3,38 @@ jQuery(document).ready(function(){
         jQuery(".menu ul ul").parent("li").children("a").append('<i class="fa-solid fa-chevron-down"></i>');
 
         jQuery(".menu ul li").click(function(){
-        jQuery("ul").slideDown(2000);
+        jQuery(this).children("ul").slideToggle();
+
+        });
+
+        var width= jQuery("body").width();
+        if(width < 768){
+
+                jQuery(".show").show();
+                jQuery(".hide").hide();
+                jQuery(".menu").hide();
+
+        }
+        else{
+
+                jQuery(".show").hide();
+                jQuery(".hide").hide();
+                jQuery(".menu").show();  
+        }
+
+        jQuery(".show").click(function(){
+
+                jQuery(".show").hide();
+                jQuery(".hide").show();
+                jQuery(".menu").show();
+               
+        });
+
+        jQuery(".hide").click(function(){
+           
+                jQuery(".show").show();
+                jQuery(".hide").hide();
+                jQuery(".menu").hide();
 
         });
 
